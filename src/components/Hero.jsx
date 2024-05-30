60abd32d7a84ea21203df08e7a585a5af937ebdf
AASTHA_2020 <modiaastha0@gmail.com>
2024-05-30 12:20:21 +0530

    final

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <div className="hero">
+            <div className="hero-inner">
+                <h1>Let's find your</h1>
+                <h1>Forever Food</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, dignissimos!</p>
+                <div>
+                    <button>Search Now</button>
+                    <button>Know more</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
